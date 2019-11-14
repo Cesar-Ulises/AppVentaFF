6951001c6075e913a212b4c975b30fef14106a6e
Cesar-Ulises <cesaralvarenga500@gmail.com>
2019-11-13 22:40:50 -0600

    Agregando animación al  SplashScreen

--- app/src/main/java/com/example/proyecto_final/splash/Splash.java
+++ app/src/main/java/com/example/proyecto_final/splash/Splash.java
@@ -23,5 +23,13 @@ public class Splash extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
 
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent (Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 7000);
     }
 }
