a222a63ecda9f2503f49aef558003da02901d379
Cesar-Ulises <cesaralvarenga500@gmail.com>
2019-11-13 22:27:21 -0600

    Agregando el SplashScreen

---
+++ app/src/main/java/com/example/proyecto_final/splash/Splash.java
@@ -0,0 +1,27 @@
+package com.example.proyecto_final.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.proyecto_final.MainActivity;
+import com.example.proyecto_final.R;
+
+public class Splash extends AppCompatActivity {
+    ImageView bgapp, clover;
+    LinearLayout textsplash, texthome, menus, textDesar;
+    Animation frombottom;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+    }
+}
