50e99c99221a34b287c2f8ff7edf58d35683e9e3
Cesar-Ulises <cesaralvarenga500@gmail.com>
2019-11-14 22:13:48 -0600

    Corrigiendo errores de diseño de la interfaz

--- app/src/main/java/com/example/proyecto_final/SplashScreen/SplashScreen.java
+++ app/src/main/java/com/example/proyecto_final/SplashScreen/SplashScreen.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -20,6 +21,24 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        frombottom = AnimationUtils.loadAnimation(this, R.anim.frombottom);
+
+
+        bgapp = (ImageView) findViewById(R.id.bgapp);
+        clover = (ImageView) findViewById(R.id.clover);
+        textsplash = (LinearLayout) findViewById(R.id.textsplash);
+        texthome = (LinearLayout) findViewById(R.id.texthome);
+        textDesar= (LinearLayout) findViewById(R.id.textDesar);
+        menus = (LinearLayout) findViewById(R.id.menus);
+
+        bgapp.animate().translationY(-1900).setDuration(800).setStartDelay(300);
+        clover.animate().alpha(0).setDuration(800).setStartDelay(600);
+        textsplash.animate().translationY(140).alpha(0).setDuration(800).setStartDelay(300);
+
+        texthome.startAnimation(frombottom);
+        textDesar.startAnimation(frombottom);
+        menus.startAnimation(frombottom);
 
         new Handler().postDelayed(new Runnable() {
             @Override
