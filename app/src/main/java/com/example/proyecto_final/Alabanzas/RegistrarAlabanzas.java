300d8f8592335d8df533bbf1a38356c10cec5ffa
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:35:19 -0600

    creamos metodo de envio de datos al servidor

--- app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
@@ -15,9 +15,12 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.proyecto_final.R;
 import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
 
 public class RegistrarAlabanzas extends AppCompatActivity {
     private EditText ettitulo, etautor, etletra;
@@ -57,7 +60,7 @@ public class RegistrarAlabanzas extends AppCompatActivity {
                     a.setAutor(etautor.getText().toString().replaceAll(" ", "%20"));
                     a.setLetra(etletra.getText().toString().replaceAll(" ", "%20"));
 
-                    //agregarAlabanza(a);
+                    agregarAlabanza(a);
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -69,4 +72,27 @@ public class RegistrarAlabanzas extends AppCompatActivity {
         });
     }
 
+
+
+
+    private  void agregarAlabanza(Alabanzas a){
+        String url = "https://appmovilgamez.000webhostapp.com/agregar.php?";
+        String parametros = "titulo="+a.getTitulo()+"&autor="+a.getAutor()+"&letra="+a.getLetra();
+        cliente.post(url + parametros, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200){
+                    Toast.makeText(RegistrarAlabanzas.this, "Alabanza agregada correctamente", Toast.LENGTH_SHORT).show();
+                    ettitulo.setText("");
+                    etautor.setText("");
+                    etletra.setText("");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+            }
+        });
+    }
 }
