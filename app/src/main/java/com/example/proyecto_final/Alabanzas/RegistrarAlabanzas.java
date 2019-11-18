b3b91b38eadbd1ed10ece2d26e926f6b93cd8688
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:50:09 -0600

    envento onclick que nos lleva al registro y declaracion de variables para el registro

--- app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
@@ -3,6 +3,7 @@ package com.example.proyecto_final.Alabanzas;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -94,5 +95,10 @@ public class RegistrarAlabanzas extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void verlista(View view) {
+        Intent intent = new Intent(this, registro_alabanzas.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
@@ -3,14 +3,23 @@ package com.example.proyecto_final.Alabanzas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.example.proyecto_final.R;
+import com.loopj.android.http.AsyncHttpClient;
 
 public class registro_alabanzas extends AppCompatActivity {
+
+    private ListView lvdatos;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_alabanzas);
+
+        lvdatos = findViewById(R.id.lvDatosR);
+
+        cliente = new AsyncHttpClient();
     }
 }
