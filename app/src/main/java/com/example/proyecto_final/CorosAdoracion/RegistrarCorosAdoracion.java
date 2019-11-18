cb5b70fe910924129b010a74ba89f6147f68ac1c
acastro99 <albertocastroascc.06@gmail.com>
2019-11-18 01:14:01 -0600

    actualizacion de campos, eliminacion de RequestCorosAdo, eliminacion de programacion anterior

--- app/src/main/java/com/example/proyecto_final/CorosAdoracion/RegistrarCorosAdoracion.java
+++ app/src/main/java/com/example/proyecto_final/CorosAdoracion/RegistrarCorosAdoracion.java
@@ -17,62 +17,14 @@ import com.example.proyecto_final.R;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class RegistrarCorosAdoracion extends AppCompatActivity implements View.OnClickListener {
-    EditText etnombre, etautor, etletra;
-    Button btnR;
+public class RegistrarCorosAdoracion extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_coros_adoracion);
 
-        etnombre = findViewById(R.id.etNombreCoroAdo);
-        etautor = findViewById(R.id.etAutorCoroAdo);
-        etletra = findViewById(R.id.etLetraCoroAdo);
-        btnR = findViewById(R.id.btnGuardarCoroAdo);
 
-        btnR.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View v) {
-        final String titulo = etnombre.getText().toString();
-        final String autor = etautor.getText().toString();
-        final String letra = etletra.getText().toString();
-
-        if (etnombre.getText().toString().length() == 0){
-            etnombre.setError("Campo Obligatorio");
-        }else if (etautor.getText().toString().length() == 0){
-            etautor.setError("Campo Obligatorio");
-        }else if (etletra.getText().toString().length() == 0){
-            etletra.setError("Campo Obligatorio");
-        }else {
-            Response.Listener<String> responseListener = new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject jsonResponse = new JSONObject(response);
-                        boolean success = jsonResponse.getBoolean("success");
-
-                        if (success ){
-                            Toast.makeText(RegistrarCorosAdoracion.this, "Registro realizado exitosamente", Toast.LENGTH_SHORT).show();
-                            etnombre.setText(null);
-                            etautor.setText(null);
-                            etletra.setText(null);
-                        }else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(RegistrarCorosAdoracion.this);
-                            builder.setMessage("Error al registrar")
-                                    .setNegativeButton("Retry", null)
-                                    .create().show();
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-            };
-            RegisterRequestCoroAdo registrarRequestCoro = new RegisterRequestCoroAdo(titulo, autor, letra, responseListener);
-            RequestQueue queue = Volley.newRequestQueue(RegistrarCorosAdoracion.this);
-            queue.add(registrarRequestCoro);
-        }
-    }
 }
