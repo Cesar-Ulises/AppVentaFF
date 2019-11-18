cfeed6943be69ce7f7c67a29c8d556353ccb8fee
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:55:06 -0600

    se crea metodo para listar los registros obtenidos

--- app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
@@ -1,13 +1,22 @@
 package com.example.proyecto_final.Alabanzas;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.proyecto_final.R;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -43,5 +52,32 @@ public class registro_alabanzas extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    private  void listarAlabanzas(String respuesta){
+        final ArrayList<Alabanzas> lista = new ArrayList<Alabanzas>();
+        try{
+            JSONArray jsonArreglo = new JSONArray(respuesta);
+            for (int i=0; i<jsonArreglo.length(); i++){
+                Alabanzas a = new Alabanzas();
+                a.setId(jsonArreglo.getJSONObject(i).getInt("id_a"));
+                a.setTitulo(jsonArreglo.getJSONObject(i).getString("titulo"));
+                a.setAutor(jsonArreglo.getJSONObject(i).getString("autor"));
+                a.setLetra(jsonArreglo.getJSONObject(i).getString("letra"));
+
+                lista.add(a);
+
+            }
+
+            ArrayAdapter<Alabanzas> a = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, lista);
+            lvdatos.setAdapter(a);
+
+
+        }catch(Exception el){
+            el.printStackTrace();
+        }
+
+
     }
 }
