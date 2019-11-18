8e74f4660df2057e6df1737ab7b723f5dd112b56
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:27:34 -0600

    declaracion de variables para envio de registro

--- app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
@@ -7,18 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.proyecto_final.R;
+import com.loopj.android.http.AsyncHttpClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegistrarAlabanzas extends AppCompatActivity {
-
+    private EditText ettitulo, etautor, etletra;
+    private Button btnRegistrar;
+    private ListView lvdatos;
+    private AsyncHttpClient cliente = new AsyncHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
