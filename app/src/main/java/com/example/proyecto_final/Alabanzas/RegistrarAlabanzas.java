2f75df8cea8722356c92b0c9c9b33cc6a2976626
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:34:15 -0600

    creamos metodo almacenar con onclick

--- app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/RegistrarAlabanzas.java
@@ -41,5 +41,32 @@ public class RegistrarAlabanzas extends AppCompatActivity {
 
     }
 
+    private void almacenarAlabanzas() {
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ettitulo.getText().toString().length()== 0 )  {
+                    ettitulo.setError("Campo Obligatorio");
+                }else if (etautor.getText().toString().length()== 0){
+                    etautor.setError("Campo Obligatorio");
+                }else  if (etletra.getText().toString().length()== 0){
+                    etletra.setError("Campo Obligatorio");
+                }else{
+                    Alabanzas a = new Alabanzas();
+                    a.setTitulo(ettitulo.getText().toString().replaceAll(" ", "%20"));
+                    a.setAutor(etautor.getText().toString().replaceAll(" ", "%20"));
+                    a.setLetra(etletra.getText().toString().replaceAll(" ", "%20"));
+
+                    //agregarAlabanza(a);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    //obtenerAlabanzas();
+                }
+            }
+        });
+    }
 
 }
