b3538e5000e54e4be63277342b80140625c30996
acastro99 <albertocastroascc.06@gmail.com>
2019-11-18 01:17:53 -0600

    declaracion de variables e igualadas a su control

--- app/src/main/java/com/example/proyecto_final/CorosAdoracion/RegistrarCorosAdoracion.java
+++ app/src/main/java/com/example/proyecto_final/CorosAdoracion/RegistrarCorosAdoracion.java
@@ -7,24 +7,36 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.example.proyecto_final.R;
+import com.loopj.android.http.AsyncHttpClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RegistrarCorosAdoracion extends AppCompatActivity {
 
+    private EditText ettituloca, etautorca, etletraca;
+    private Button btnRegistrarca;
+    private ListView lvdatosca;
+    private AsyncHttpClient clienteca = new AsyncHttpClient();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_coros_adoracion);
 
+        ettituloca = findViewById(R.id.ettituloca);
+        etautorca = findViewById(R.id.etautorca);
+        etletraca = findViewById(R.id.etletraca);
 
+        btnRegistrarca = findViewById(R.id.btnRegistrarca);
+
+        clienteca = new AsyncHttpClient();
     }
 
 }
