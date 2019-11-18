9115a0863f25ac13685cac10c83eeb75dcf857f0
acastro99 <albertocastroascc.06@gmail.com>
2019-11-17 23:56:05 -0600

    se crea metodo onclick para eliminar y mostrar

--- app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
+++ app/src/main/java/com/example/proyecto_final/Alabanzas/registro_alabanzas.java
@@ -73,7 +73,56 @@ public class registro_alabanzas extends AppCompatActivity {
             ArrayAdapter<Alabanzas> a = new ArrayAdapter(this, R.layout.support_simple_spinner_dropdown_item, lista);
             lvdatos.setAdapter(a);
 
+            lvdatos.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
 
+                    Alabanzas a = lista.get(position);
+                    String url = "https://appmovilgamez.000webhostapp.com/eliminar.php?id_a="+a.getId();
+
+                    cliente.post(url, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            if (statusCode == 200){
+                                Toast.makeText(registro_alabanzas.this, "Alabanza liminada Correctamente", Toast.LENGTH_SHORT).show();
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                obtenerAlabanzas();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+
+                        }
+                    });
+
+                    return true;
+                }
+            });
+
+
+            lvdatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    Alabanzas a = lista.get(position);
+                    StringBuffer b = new StringBuffer();
+                    b.append("ID: " + a.getId() + "\n");
+                    b.append("TITULO: " + a.getTitulo() + "\n");
+                    b.append("AUTOR: " + a.getTitulo() + "\n");
+                    b.append("LETRA: " + a.getLetra() + "\n");
+
+                    AlertDialog.Builder al = new AlertDialog.Builder(registro_alabanzas.this);
+                    al.setCancelable(true);
+                    al.setTitle("Detalle");
+                    al.setMessage(a.tostring());
+                    //al.setIcon(R.drawable.xx);
+                    al.show();
+                }
+            });
         }catch(Exception el){
             el.printStackTrace();
         }
